f43a44983ceaea43bac57885305a551a9d42f9d3
IDGS-803-20001577 <20001577@alumnos.utleon.edu.mx>
2023-02-25 13:02:20 -0600

    Trabajo correspondiente a 25 de febrero 2023

--- app.py
+++ app.py
@@ -1,8 +1,45 @@
 from flask import Flask, render_template
 from flask import request
+from flask_wtf.csrf import CSRFProtect
+from flask import make_response
+from flask import flash
 
 import forms
 app=Flask(__name__)
+app.config['SECRET_KEY']="Esta es la clave encriptada"
+csrf = CSRFProtect()
+
+@app.errorhandler(404)
+def no_encontrada(e):
+    return render_template('404.html'),404
+
+@app.route("/cookies",methods=['GET','POST'])
+def cookies():
+    reg_user = forms.loginForm(request.form)
+    datos=''
+    if request.method=='POST' and reg_user.validate():
+        user=reg_user.username.data
+        passw=reg_user.password.data
+        datos = user+'@'+passw
+        success_message = 'Bienvenido prro {}'.format(user)
+        flash(success_message)
+
+    response=make_response(render_template('cookies.html',form=reg_user))
+    if len(datos)>0:
+        response.set_cookie('datos_user',datos)
+
+    return  response
+
+
+@app.route("/saludo",methods=['GET','POST'])
+def saludo():
+    valor_cookie = request.cookies.get('datos_user')
+    nombres = valor_cookie.split('@')
+    return render_template('saludo.html',nomb=nombres[0])
+
+
+
+# -----------------------------------------------------------------------------------------
 
 @app.route("/formulario",methods=['GET','POST'])
 def formulario():
@@ -13,7 +50,7 @@ def formulario():
 @app.route("/Alumnos", methods=['GET','POST'])
 def alumnos():
     alum_form = forms.UserForm(request.form)
-    if request.method=='POST':
+    if request.method=='POST' and alum_form.validate():
         print(alum_form.matricula.data)
         print(alum_form.nombre.data)
     return render_template('alumnos.html', form= alum_form)
@@ -37,21 +74,22 @@ def generate():
 def calculate():
     num_casillas = int(request.form['num_casillas'])
     total = 0
-    multiplicacion = 1
     promedio = 0
+
+
+   
 
 
     for i in range(num_casillas):
         num = int(request.form['num'+str(i)])
         total += num
 
-        
-        multiplicacion *= num
 
         promedio = total / num_casillas  
 
     
-    return render_template('resultado.html', total=total, multiplicacion=multiplicacion, promedio=promedio)
+    return render_template('resultado.html', minimo=minimo ,promedio=promedio)
 
 if __name__=="_main_":
+    csrf.init_app(app)
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -2,10 +2,31 @@ from wtforms import Form
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, FieldList, FormField, SelectField, RadioField
 from wtforms.fields import EmailField
+from wtforms import validators
 
+
+def mi_validacion(form,field):
+    if len(field.data)==0 :
+        raise validators.ValidationError('El campo no tiene datos')
 
 class UserForm(Form):
-    matricula=StringField('Matricula')
-    nombre = StringField('Nombre')
-    apaterno=StringField('Apaterno')
+    matricula=StringField('Matricula',[
+        validators.DataRequired(message='El Campo es Requerido'),
+        validators.length(min=4,max=8, message='No cumple la longitud esperada')
+        ])
+    nombre = StringField('Nombre',[
+        validators.DataRequired(message='El Campo es Requerido')
+        ])
+    apaterno=StringField('Apaterno',[mi_validacion])
     email = EmailField('correo')
+
+
+class loginForm(Form):
+    username=StringField('usuario',[
+        validators.DataRequired(message='El Campo es Requerido'),
+        validators.length(min=4,max=8, message='No cumple la longitud esperada')
+        ])
+    password = StringField('password',[
+        validators.DataRequired(message='El Campo es Requerido'),
+        validators.length(min=4,max=8, message='No cumple la longitud esperada')
+        ])
